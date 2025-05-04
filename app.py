2c4bae4d4daeffc849678ddb1ab86c9aec1c689d
SofiChamorro <mschamorry@eafit.edu.co>
2025-05-04 12:26:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import streamlit as st
-st.title("Gatos en adopción")
+st.title("Nube en adopción")
 st.image("https://hospitalveterinariodonostia.com/wp-content/uploads/2022/02/Personalidad-gatos.png", caption="Imagen desde la web")
