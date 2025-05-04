fb7ecab6bdcae348a8cc7ba33b58d573f9995e21
SofiChamorro <mschamorry@eafit.edu.co>
2025-05-04 12:27:41 -0500

    Update pagina2.py

--- pages/pagina2.py
+++ pages/pagina2.py
@@ -1,2 +1,3 @@
 import streamlit as st 
-st.title ("Pagina2")
+st.title ("Manchita en adopción")
+st.image("https://images.unsplash.com/photo-1591621211041-a3d8b1a4e43a?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2F0byUyMGJlYmV8ZW58MHx8MHx8fDA%3D")
