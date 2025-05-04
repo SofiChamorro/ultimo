58bce21f9c6233bb72de9cca1aecd034e04d9748
SofiChamorro <mschamorry@eafit.edu.co>
2025-05-04 12:24:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
 import streamlit as st
 st.title("pagina1")
+st.image("https://hospitalveterinariodonostia.com/wp-content/uploads/2022/02/Personalidad-gatos.png", caption="Imagen desde la web", use_column_width=True)
